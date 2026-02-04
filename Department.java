25bde68d6e6d58c43164b9a8c483e9ac7ded431c
s68122250095-cpu <s68122250095@ssru.ac.th>
2026-02-04 19:30:30 +0700

    Create Department class with basic CRUD methods

---
+++ Department.java
@@ -0,0 +1,8 @@
+public class Department {
+    private int department_id;
+    private String department_name;
+
+    public void insertDepartment() {}
+    public void updateDepartment() {}
+    public void deleteDepartment() {}
+}
