09e61495324a2cf8450143a7a63e0ad76a192d01
s68122250095-cpu <s68122250095@ssru.ac.th>
2026-02-04 19:39:18 +0700

    Class

---
+++ Report.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Report {
+    private int report_code;
+    private Date report_date;
+
+    public void viewReports() {}
+    public void createReports() {}
+}
