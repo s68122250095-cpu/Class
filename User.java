bb075b98a3cefa225d3e2c01fe6a9c92490f4a6e
s68122250095-cpu <s68122250095@ssru.ac.th>
2026-02-04 19:39:47 +0700

    Class

---
+++ User.java
@@ -0,0 +1,20 @@
+public class User {
+    private int user_id;
+    private int department_id;
+    private String user_username;
+    private String user_password;
+    private String user_name;
+    private Gender user_gender;
+    private String user_email;
+    private RoleUser user_role;
+
+    private Department department;
+
+    public void userLogin() {}
+    public void addProposal() {}
+    public void viewProposal() {}
+    public void confirmProposal() {}
+    public void creatReports() {}
+    public void viewReports() {}
+    public void updateDepartment() {}
+}
